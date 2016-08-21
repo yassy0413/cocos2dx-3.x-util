1b67f53b7cf6f5d41966344ff33ea1f165a8fbce
yassy <wiren0413@gmail.com>
2016-08-22 02:05:44 +0900

    ラムダ式TableView

---
+++ CCTableViewLambda.cpp
@@ -0,0 +1,108 @@
+#include "CCTableViewLambda.h"
+
+NS_CC_EXT_BEGIN
+
+#pragma mark -- TableViewLambda
+
+TableViewLambda* TableViewLambda::create(Size size,
+                                         OnNumberOfCellsInTableView numberOfCellsInTableView,
+                                         OnTableCellSizeForIndex tableCellSizeForIndex,
+                                         OnTableCellAtIndex tableCellAtIndex)
+{
+    auto pRet = new (std::nothrow) TableViewLambda();
+    if( pRet && pRet->initWithViewSize(size) ){
+        pRet->autorelease();
+        pRet->onNumberOfCellsInTableView = numberOfCellsInTableView;
+        pRet->onTableCellSizeForIndex = tableCellSizeForIndex;
+        pRet->onTableCellAtIndex = tableCellAtIndex;
+        pRet->setDataSource(pRet);
+        pRet->_updateCellPositions();
+        pRet->_updateContentSize();
+        return pRet;
+    }
+    delete pRet;
+    return nullptr;
+}
+
+
+TableViewLambda::TableViewLambda()
+: onNumberOfCellsInTableView(nullptr)
+, onTableCellSizeForIndex(nullptr)
+, onTableCellAtIndex(nullptr)
+{}
+
+TableViewLambda::~TableViewLambda(){
+}
+
+
+#pragma mark -- TableViewDataSource
+
+ssize_t TableViewLambda::numberOfCellsInTableView(cocos2d::extension::TableView *table){
+    CC_ASSERT( onNumberOfCellsInTableView );
+    return onNumberOfCellsInTableView();
+}
+
+cocos2d::Size TableViewLambda::tableCellSizeForIndex(cocos2d::extension::TableView *table, ssize_t idx){
+    CC_ASSERT( onNumberOfCellsInTableView );
+    return onTableCellSizeForIndex(idx);
+}
+
+cocos2d::extension::TableViewCell* TableViewLambda::tableCellAtIndex(cocos2d::extension::TableView *table, ssize_t idx){
+    CC_ASSERT( onNumberOfCellsInTableView );
+    return onTableCellAtIndex(idx, table);
+}
+
+
+#pragma mark -- TableViewEasyLambda
+
+TableViewEasyLambda* TableViewEasyLambda::create(Size size,
+                                                 OnNumberOfCellsInTableView numberOfCellsInTableView,
+                                                 OnTableCellSizeForIndex tableCellSizeForIndex,
+                                                 OnTableNodeAtIndex tableNodeAtIndex)
+{
+    auto pRet = new (std::nothrow) TableViewEasyLambda();
+    if( pRet && pRet->initWithViewSize(size) ){
+        pRet->autorelease();
+        pRet->onNumberOfCellsInTableView = numberOfCellsInTableView;
+        pRet->onTableCellSizeForIndex = tableCellSizeForIndex;
+        pRet->onTableNodeAtIndex = tableNodeAtIndex;
+        pRet->setDataSource(pRet);
+        pRet->_updateCellPositions();
+        pRet->_updateContentSize();
+        return pRet;
+    }
+    delete pRet;
+    return nullptr;
+}
+
+
+TableViewEasyLambda::TableViewEasyLambda()
+: onTableNodeAtIndex(nullptr)
+{
+    onTableCellAtIndex = [this](int idx, TableView *table){
+        CC_ASSERT(onTableNodeAtIndex);
+        
+        auto cell = (TableViewCell*)table->dequeueCell();
+        if( !cell ){
+            cell = new (std::nothrow) TableViewCell();
+            cell->autorelease();
+        }
+        
+        auto container = cell->getChildByTag(0);
+        if( !container ){
+            container = onTableNodeAtIndex(idx, nullptr);
+            container->setTag( 0 );
+            cell->addChild( container );
+        }else{
+            onTableNodeAtIndex(idx, container);
+        }
+        
+        return cell;
+    };
+}
+
+TableViewEasyLambda::~TableViewEasyLambda(){
+}
+
+
+NS_CC_EXT_END
---
+++ CCTableViewLambda.h
@@ -0,0 +1,78 @@
+/****************************************************************************
+ Copyright (c) Yassy
+ https://github.com/yassy0413/cocos2dx-3.x-util
+ ****************************************************************************/
+#ifndef __CCTABLEVIEW_LAMBDA_H__
+#define __CCTABLEVIEW_LAMBDA_H__
+
+#include "cocos2d.h"
+#include "cocos-ext.h"
+
+
+NS_CC_EXT_BEGIN
+
+/**
+ * コールバックをラムダ式で定義できる TableView
+ */
+class TableViewLambda
+: public TableView
+, public TableViewDataSource
+{
+public:
+    // callbacks
+    using OnNumberOfCellsInTableView = std::function<ssize_t()>;
+    using OnTableCellSizeForIndex = std::function<cocos2d::Size(int idx)>;
+    using OnTableCellAtIndex = std::function<cocos2d::extension::TableViewCell*(int idx, cocos2d::extension::TableView *table)>;
+    
+    /**
+     * Creates a table view with lambda.
+     */
+    static TableViewLambda* create(Size size,
+                                   OnNumberOfCellsInTableView numberOfCellsInTableView,
+                                   OnTableCellSizeForIndex tableCellSizeForIndex,
+                                   OnTableCellAtIndex tableCellAtIndex);
+    
+    // TableViewDataSource
+    virtual cocos2d::Size tableCellSizeForIndex(cocos2d::extension::TableView *table, ssize_t idx) override;
+    virtual cocos2d::extension::TableViewCell* tableCellAtIndex(cocos2d::extension::TableView *table, ssize_t idx) override;
+    virtual ssize_t numberOfCellsInTableView(cocos2d::extension::TableView *table) override;
+    
+CC_CONSTRUCTOR_ACCESS:
+    TableViewLambda();
+    virtual ~TableViewLambda();
+	
+protected:
+    OnNumberOfCellsInTableView onNumberOfCellsInTableView;
+    OnTableCellSizeForIndex onTableCellSizeForIndex;
+    OnTableCellAtIndex onTableCellAtIndex;
+};
+
+/**
+ * Cellのインスタンス管理を簡略化したコールバックを搭載
+ */
+class TableViewEasyLambda
+: public TableViewLambda
+{
+public:
+    // callback
+    using OnTableNodeAtIndex = std::function<cocos2d::Node*(int idx, cocos2d::Node *node)>;
+    
+    /**
+     * Creates a table view with lambda.
+     */
+    static TableViewEasyLambda* create(Size size,
+                                       OnNumberOfCellsInTableView numberOfCellsInTableView,
+                                       OnTableCellSizeForIndex tableCellSizeForIndex,
+                                       OnTableNodeAtIndex tableNodeAtIndex);
+    
+CC_CONSTRUCTOR_ACCESS:
+    TableViewEasyLambda();
+    virtual ~TableViewEasyLambda();
+    
+protected:
+    OnTableNodeAtIndex onTableNodeAtIndex;
+};
+
+NS_CC_EXT_END
+
+#endif
