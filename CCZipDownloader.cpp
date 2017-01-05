ec4dc09638e5d883910afa71e53c745bb59bc683
yassy <wiren0413@gmail.com>
2017-01-06 02:05:17 +0900

    cocosのASyncTaskを使った簡易実装に変更

--- CCZipDownloader.cpp
+++ CCZipDownloader.cpp
@@ -7,6 +7,68 @@
 
 NS_CC_EXT_BEGIN
 
+static void pushToWriteFile(const ccZipDownloaderCallback& callback, const std::string& path, unsigned char* data, ssize_t size){
+    CCLOG("pushToWriteFile: %s", path.c_str());
+    // 別スレッドで実行されるタスク
+    auto task = [path, data, size](){
+        if( data ){
+            FILE* file = fopen(path.c_str(), "wb");
+            fwrite(data, size, 1, file);
+            fclose(file);
+            CCLOG("endOfWriteFile: %s", path.c_str());
+            
+            // zipfile->getFileData で確保されたメモリを開放する
+            free(data);
+        }
+    };
+    // 最後にUIスレッドで実行されるタスク
+    auto finished = [callback](void*){
+        if(callback){ callback(true); }
+    };
+    // 非同期タスクの開始 (TASK_IOのスレッドキューへ積まれる)
+    AsyncTaskPool::getInstance()->enqueue(AsyncTaskPool::TaskType::TASK_IO, finished, nullptr, task);
+}
+
+static void pushToUnzip(const ccZipDownloaderCallback& callback, const std::string& outdir, network::HttpResponse* response){
+    // 別スレッドで実行されるタスク
+    auto task = [callback, outdir, response](){
+        
+        // zipに含まれるファイル情報リストを取得
+        ZipFile* zipfile = ZipFile::createWithBuffer(response->getResponseData()->data(),
+                                                     response->getResponseData()->size());
+        for( std::string filename = zipfile->getFirstFilename(); !filename.empty(); filename = zipfile->getNextFilename() ){
+            if( *filename.rbegin() == '/' ){
+                
+                // It's a directory.
+                FileUtils::getInstance()->createDirectory( outdir + filename );
+                
+            }else{
+                
+                // It's a file.
+                ssize_t filesize;
+                unsigned char* filedata = zipfile->getFileData( filename, &filesize );
+                // ファイルデータが取得できたので、書き込みを行うスレッドへ送る
+                pushToWriteFile(nullptr, outdir + filename, filedata, filesize);
+                
+            }
+        }
+        delete zipfile;
+        
+        // 終了判定用に空データを送る
+        pushToWriteFile(callback, "", nullptr, 0);
+    };
+    // 最後にUIスレッドで実行されるタスク
+    auto finished = [response](void*){
+        response->release();
+    };
+    // unzipタスク実行中に破棄されないよう保護する
+    response->retain();
+    // 非同期タスクの開始 (TASK_OTHERのスレッドキューへ積まれる)
+    AsyncTaskPool::getInstance()->enqueue(AsyncTaskPool::TaskType::TASK_OTHER, finished, nullptr, task);
+}
+
+#pragma mark - ZipDownloader Class
+
 static ZipDownloader *s_pZipDownloader = nullptr; // pointer to singleton
 
 ZipDownloader* ZipDownloader::getInstance(){
@@ -21,222 +83,25 @@ void ZipDownloader::destroyInstance(){
 }
 
 ZipDownloader::ZipDownloader()
-: _numWorkingTask(0)
-, _numWorkingDownloadTask(0)
-, _downloadedDataSize(0)
-, _limitDownloadedDataSize(10*1024*1024)
-, _limitDownloadConcurrency(3)
-{
-    _downloadThread = std::thread(CC_CALLBACK_0(ZipDownloader::downloadThread, this));
-    _unzipThread = std::thread(CC_CALLBACK_0(ZipDownloader::unzipThread, this));
-}
+{}
 
 ZipDownloader::~ZipDownloader(){
-    CC_ASSERT( numWorkingTask() == 0 );
-    
-    _downloadQueueMutex.lock();
-    _downloadQueue.push( nullptr );
-    _downloadQueueMutex.unlock();
-    _downloadSleepCondition.notify_one();
-    
-    _unzipQueueMutex.lock();
-    _unzipQueue.push( nullptr );
-    _unzipQueueMutex.unlock();
-    _unzipSleepCondition.notify_one();
-    
-    _downloadThread.join();
-    _unzipThread.join();
-    
-    s_pZipDownloader = nullptr;
 }
 
-#pragma mark -- Download Work
-
-void ZipDownloader::download(const std::string& url, ccZipDownloaderCallback callback){
-    
-    ++_numWorkingTask;
-    
-    auto pUnit = new DownloadUnit();
-    pUnit->_callback = callback;
-    pUnit->_url = url;
-    
-    _downloadQueueMutex.lock();
-    _downloadQueue.push( pUnit );
-    _downloadQueueMutex.unlock();
-    _downloadSleepCondition.notify_one();
-}
-
-void ZipDownloader::downloadThread(){
-    
-    while(1)
-    {
-        DownloadUnit* pUnit = nullptr;
-        
-        // タスク発行待ち
-        {
-            std::lock_guard<std::mutex> lock( _downloadQueueMutex );
-            while( _downloadQueue.empty() || (_numWorkingDownloadTask >= _limitDownloadConcurrency) || (_downloadedDataSize > _limitDownloadedDataSize) ){
-                _downloadSleepCondition.wait( _downloadQueueMutex );
-            }
-            pUnit = _downloadQueue.front();
-            _downloadQueue.pop();
+void ZipDownloader::download(const std::string& url, const std::string& outdir, const ccZipDownloaderCallback& callback){
+    auto req = new (std::nothrow) network::HttpRequest();
+    req->setRequestType(network::HttpRequest::Type::GET);
+    req->setUrl(url);
+    req->setResponseCallback([callback, outdir](network::HttpClient* client, network::HttpResponse* response){
+        if( response->isSucceed() ){
+            // ダウンロードしたzipファイルを展開スレッドへ送る
+            pushToUnzip(callback, outdir, response);
+        }else{
+            callback(false);
         }
-        
-        // 終了検知
-        if( pUnit == nullptr ) {
-            break;
-        }
-        
-        // HttpRequest の発行
-        auto req = new network::HttpRequest();
-        req->setRequestType( network::HttpRequest::Type::GET );
-        req->setUrl( pUnit->_url.c_str() );
-        req->setResponseCallback( CC_CALLBACK_2(ZipDownloader::httpRequestCallback, this) );
-        req->setUserData( pUnit );
-        
-        ++_numWorkingDownloadTask;
-        network::HttpClient::getInstance()->sendImmediate( req );
-    }
-}
-
-void ZipDownloader::httpRequestCallback(network::HttpClient* client, network::HttpResponse* response){
-    //CCLOG("ZipDownloader::httpRequestCallback: %s", response->getHttpRequest()->getUrl());
-    
-    if( !response->isSucceed() ){
-        CC_ASSERT(0);
-        return;
-    }
-    
-    --_numWorkingDownloadTask;
-    
-    auto pUnit = new UnzipUnit();
-    pUnit->_url = response->getHttpRequest()->getUrl();
-    pUnit->_data = &response->getResponseData()->at(0);
-    pUnit->_datasize = response->getResponseData()->size();
-    pUnit->_pHttpResponse = response;
-    pUnit->_pHttpResponse->retain();
-    
-    auto dlUnit = (DownloadUnit*)response->getHttpRequest()->getUserData();
-    pUnit->_callback = dlUnit->_callback;
-    delete dlUnit;
-    
-    _downloadedDataSize += pUnit->_datasize;
-    
-    _unzipQueueMutex.lock();
-    _unzipQueue.push( pUnit );
-    _unzipQueueMutex.unlock();
-    _unzipSleepCondition.notify_one();
-}
-
-#pragma mark -- Unzip Work
-
-void ZipDownloader::unzip(const std::string& url, const void* data, ssize_t datasize, ccZipDownloaderCallback callback){
-    
-    ++_numWorkingTask;
-    
-    UnzipUnit* pUnit = new UnzipUnit();
-    pUnit->_url = url;
-    pUnit->_data = data;
-    pUnit->_datasize = datasize;
-    pUnit->_pHttpResponse = nullptr;
-    pUnit->_callback = callback;
-    
-    _downloadedDataSize += pUnit->_datasize;
-    
-    _unzipQueueMutex.lock();
-    _unzipQueue.push( pUnit );
-    _unzipQueueMutex.unlock();
-    _unzipSleepCondition.notify_one();
-}
-
-void ZipDownloader::unzipThread(){
-    
-    auto scheduler = Director::getInstance()->getScheduler();
-    
-    // 出力先のルートパスを取得
-    const std::string writablePath( cocos2d::FileUtils::getInstance()->getWritablePath() );
-    
-    while(1)
-    {
-        UnzipUnit* pUnit = nullptr;
-        
-        // タスク発行待ち
-        {
-            std::lock_guard<std::mutex> lock( _unzipQueueMutex );
-            while( _unzipQueue.empty() ){
-                _unzipSleepCondition.wait( _unzipQueueMutex );
-            }
-            pUnit = _unzipQueue.front();
-            _unzipQueue.pop();
-        }
-        
-        _downloadSleepCondition.notify_one();
-        
-        // 終了検知
-        if( pUnit == nullptr ) {
-            break;
-        }
-        
-        // zipに含まれるファイル情報リストを取得して展開
-        cocos2d::ZipFile* zipfile = cocos2d::ZipFile::createWithBuffer( pUnit->_data, pUnit->_datasize );
-        for( std::string filename = zipfile->getFirstFilename(); !filename.empty(); filename = zipfile->getNextFilename() ){
-            if( *filename.rbegin() == '/' ){
-                
-                // It's a directory.
-                cocos2d::FileUtils::getInstance()->createDirectory( writablePath + filename );
-                
-            }else{
-                
-                // It's a file.
-                ssize_t filesize;
-                unsigned char* filedata = zipfile->getFileData( filename, &filesize );
-                {
-                    FILE* file = fopen( (writablePath + filename).c_str(), "wb" );
-                    fwrite( filedata, filesize, 1, file );
-                    fclose( file );
-                }
-                free( filedata );
-                
-            }
-        }
-        delete zipfile;
-        
-        _downloadedDataSize -= pUnit->_datasize;
-        
-        // UIスレッドからのコールバック設定
-        _responseQueueMutex.lock();
-        _responseQueue.push( pUnit );
-        _responseQueueMutex.unlock();
-        
-        scheduler->performFunctionInCocosThread(CC_CALLBACK_0(ZipDownloader::dispatchResponseCallbacks, this));
-    }
-}
-
-void ZipDownloader::dispatchResponseCallbacks(){
-    
-    UnzipUnit* pUnit = nullptr;
-    
-    _responseQueueMutex.lock();
-    if( !_responseQueue.empty() ){
-        pUnit = _responseQueue.front();
-        _responseQueue.pop();
-    }
-    _responseQueueMutex.unlock();
-    
-    if( pUnit ){
-        if( pUnit->_callback ){
-            pUnit->_callback( pUnit->_url, pUnit->_data );
-        }
-        delete pUnit;
-    }
-    
-    --_numWorkingTask;
-}
-
-ZipDownloader::UnzipUnit::~UnzipUnit(){
-    if( _pHttpResponse ){
-        _pHttpResponse->release();
-    }
+    });
+    network::HttpClient::getInstance()->send( req );
+    req->release();
 }
 
 NS_CC_EXT_END
--- CCZipDownloader.h
+++ CCZipDownloader.h
@@ -6,21 +6,11 @@
 #define __CC_ZIP_DOWNLOADER_H__
 
 #include "cocos2d.h"
-#include "cocos-ext.h"
-#include <thread>
-#include <queue>
-
-NS_CC_BEGIN
-namespace network {
-    class HttpClient;
-    class HttpResponse;
-}
-NS_CC_END
+#include "ExtensionMacros.h"
 
 NS_CC_EXT_BEGIN
 
-class ZipDownloader;
-typedef std::function<void(const std::string& url, const void* data)> ccZipDownloaderCallback;
+typedef std::function<void(bool succeeded)> ccZipDownloaderCallback;
 
 /**
  * zipファイルのダウンロードと展開
@@ -39,75 +29,13 @@ public:
     static void destroyInstance();
     
     /**
-     * urlで指定されたzipファイルをダウンロードし、WritablePathへ展開する
+     * urlで指定されたzipファイルをダウンロードし、outdirへ展開する
      */
-    void download(const std::string& url, ccZipDownloaderCallback callback);
-    
-    /**
-     * dataで指定されたzipのバイナリデータを、WritablePathへ展開する
-     * @param url 未使用。コールバックで渡されるのみ
-     */
-    void unzip(const std::string& url, const void* data, ssize_t datasize, ccZipDownloaderCallback callback);
-    
-    /**
-     * 実行中のタスク数を取得
-     */
-    inline size_t numWorkingTask() const { return _numWorkingTask; }
-    
-    /**
-     * ダウンロードが極端に早い場合のメモリ使用量肥大を防ぐ為、ダウンロード要求に制限をかける閾値
-     *   ダウンロード済みでメモリ上に待機している合計データサイズが
-     *   limitで設定された値を超えている場合は、ダウンロード要求が待機される
-     */
-    void setLimitDownloadedDataSize(size_t limit){ _limitDownloadedDataSize = limit; }
-    
-    /**
-     * ダウンロード同時進行数を設定
-     */
-    void setLimitDownloadConcurrency(size_t limit){ _limitDownloadConcurrency = limit; }
+    void download(const std::string& url, const std::string& outdir, const ccZipDownloaderCallback& callback);
     
 private:
     ZipDownloader();
     virtual ~ZipDownloader();
-    
-    void downloadThread();
-    void httpRequestCallback(network::HttpClient* client, network::HttpResponse* response);
-    void unzipThread();
-    void dispatchResponseCallbacks();
-    
-    struct DownloadUnit {
-        ccZipDownloaderCallback _callback;
-        std::string _url;
-    };
-    
-    struct UnzipUnit {
-        ccZipDownloaderCallback _callback;
-        network::HttpResponse* _pHttpResponse;
-        std::string _url;
-        const void* _data;
-        ssize_t _datasize;
-        
-        ~UnzipUnit();
-    };
-    
-    std::thread _downloadThread;
-    std::queue<DownloadUnit*> _downloadQueue;
-    std::mutex _downloadQueueMutex;
-    std::condition_variable_any _downloadSleepCondition;
-    
-    std::thread _unzipThread;
-    std::queue<UnzipUnit*> _unzipQueue;
-    std::mutex _unzipQueueMutex;
-    std::condition_variable_any _unzipSleepCondition;
-    
-    std::queue<UnzipUnit*> _responseQueue;
-    std::mutex _responseQueueMutex;
-    
-    size_t _numWorkingTask;
-    size_t _numWorkingDownloadTask;
-    size_t _downloadedDataSize;
-    size_t _limitDownloadedDataSize;
-    size_t _limitDownloadConcurrency;
 };
 
 NS_CC_EXT_END
